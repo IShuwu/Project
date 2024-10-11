b9e3266f19b0f449fc99781960ab3ee2f20cbd3f
Ishuwu <you@example.com>
2024-10-11 11:52:15 +0530

    ishu

---
+++ script.js
@@ -0,0 +1,16 @@
+// document.getElementById("title").innerHTML = "New Title";
+// const a = document.getElementById("myDIV");
+const button = document.querySelector(".checked");
+console.log(button);
+
+var list = docuement.querySelector("ul");
+list.addEventListener(
+    "click",
+    function (ev) {
+        if (ev.target.tagName==="LI") {
+            ev.target.classList.toggle("checked");
+        }
+    },
+    false
+);
+    
